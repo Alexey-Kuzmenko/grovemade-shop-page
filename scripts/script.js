b73eaaa75a105c85d939075a7325e8612d144e05
Alexey Kuzmenko <alesha1103348@gmail.com>
2022-04-21 16:32:06 +0300

    fixed bugs with link in cover block

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,17 @@
 
 // * header UI elements
+const body = document.body
 const header = document.querySelector('.header')
 const nav = document.querySelector('.nav')
 const chart = document.querySelector('.chart')
+// const coverLink = document.querySelector('.cover-link')
 
-header.addEventListener('mouseover', function ({ target }) {
+body.addEventListener('mouseover', function ({ target }) {
     if (target.tagName === 'A') {
         target.classList.add('active')
     }
 })
-header.addEventListener('mouseout', function ({ target }) {
+body.addEventListener('mouseout', function ({ target }) {
     if (target.tagName === 'A') {
         target.classList.remove('active')
     }
