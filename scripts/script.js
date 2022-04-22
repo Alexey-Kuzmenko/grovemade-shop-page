b9c97c630859b9fb4747c570705a576ae0b8b659
Alexey Kuzmenko <alesha1103348@gmail.com>
2022-04-22 17:33:06 +0300

    added button wich scroll to top and added this button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,35 @@
+(function () {
 
-// * header UI elements
-const body = document.body
-const header = document.querySelector('.header')
-const nav = document.querySelector('.nav')
-const chart = document.querySelector('.chart')
-// const coverLink = document.querySelector('.cover-link')
+    // * header UI elements
+    const body = document.body
+    const header = document.querySelector('.header')
+    const nav = document.querySelector('.nav')
+    const chart = document.querySelector('.chart')
+    const toTopBtn = document.querySelector('.to-top-btn')
 
-body.addEventListener('mouseover', function ({ target }) {
-    if (target.tagName === 'A') {
-        target.classList.add('active')
-    }
-})
-body.addEventListener('mouseout', function ({ target }) {
-    if (target.tagName === 'A') {
-        target.classList.remove('active')
-    }
-})
+    body.addEventListener('mouseover', function ({ target }) {
+        if (target.tagName === 'A') {
+            target.classList.add('active')
+        }
+    })
+    body.addEventListener('mouseout', function ({ target }) {
+        if (target.tagName === 'A') {
+            target.classList.remove('active')
+        }
+    })
+
+    toTopBtn.addEventListener('click', function (event) {
+        // event.preventDefault()
+
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        })
+
+    }, {
+        once: false,
+        passive: false,
+        capture: false,
+    })
+
+})()
